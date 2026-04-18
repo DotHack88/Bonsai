a122c8767bef859c420c0e38a3189665361cab32
N@poli_88 <dothack@live.it>
2026-04-18 22:30:30 +0200

    Update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'it.bonsai.app',
+  appName: 'bonsai-app',
+  webDir: 'build'
+};
+
+export default config;
